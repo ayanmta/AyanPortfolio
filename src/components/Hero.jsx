a20ce419be0af89ef9c1970ebde370203e0d28d9
ayanmta <mehtaayan18@gmail.com>
2023-09-22 12:58:52 +0530

    added 3d modal

---
+++ src/components/Hero.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row  items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 bg-gradient-to-bl from-[#915eff] via-[#2e1464] to-[#0c051d]" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            {" "}
+            Hey! &nbsp;
+            <span className="text-[#915eff]"> I'm Ayan,</span>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              A Developer
+            </p>
+          </h1>
+        </div>
+      </div>
+      <ComputersCanvas />
+      <div className="absolute sm:bottom-10 bottom-4 flex w-full justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] border-4 rounded-3xl border-secondary flex items-start justify-center p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "loop",
+                duration: 1.5,
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable react/no-unknown-property */
+import React from "react";
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, Stars, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../CanvasLoader";
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const lumen = useGLTF("./lumen_64__computer/scene.gltf");
+  console.log(computer);
+  return (
+    <mesh>
+      <hemisphereLight intensity={2} groundColor="black" />
+      <spotLight
+        position={[0, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={2}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1.5} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.6 : 0.85}
+        position={isMobile ? [0, -2.5, -1] : [-0.15, -2.25, -0.8]}
+        rotation={[-0.01, -0.3, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputerCanvas = () => {
+  const [isMobile, setIsmobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+    setIsmobile(mediaQuery.matches);
+    console.log(mediaQuery, "mediaevent");
+    const handleMediaQueryChange = (event) => {
+      console.log(event, "mediaevent");
+
+      setIsmobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [30, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputerCanvas;
