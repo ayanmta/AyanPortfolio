97f8d5103fac6c98f23ee12a3d736aa0a300d8ee
ayanmta <mehtaayan18@gmail.com>
2023-10-28 22:34:13 +0530

    adding prod dist folder

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
       <div className="absolute sm:bottom-10 bottom-4 flex w-full justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] border-4 rounded-3xl border-secondary flex items-start justify-center p-2">
-            <motion.dev
+            <motion.div
               animate={{ y: [0, 24, 0] }}
               transition={{
                 repeat: Infinity,
