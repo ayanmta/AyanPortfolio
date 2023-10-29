5967a38f93fda1902ebcbb91ff5e31c8bdd88035
ayanmta <mehtaayan18@gmail.com>
2023-10-29 13:16:09 +0530

    minor fixes stage 3

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import backend from "./backend.png";
+import ayanhero from "./ayanHero.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
@@ -33,6 +34,7 @@ import tripguide from "./tripguide.png";
 export {
   logo,
   backend,
+  ayanhero,
   creator,
   mobile,
   web,
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,38 @@ import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../style";
 import { ComputersCanvas } from "./canvas";
+import { ayanhero } from "../assets";
 
 const Hero = () => {
+  const myDescription = [
+    "Web Developer",
+    "Orchardist",
+    "Science Enthusiast",
+    "Physics Lover",
+    "Painter",
+    "Space Enthusiast",
+    "Trekking Enthusiast",
+    "Traveler",
+    "Mountain Lover",
+    "Music Enthusiast",
+  ];
+  let index = 0;
+  let charIndex = 0;
+  const descriptorElement = document.getElementById("myDescription");
+  const typewriter = () => {
+    if (index < myDescription.length) {
+      const descriptor = myDescription[index];
+      if (charIndex < descriptor.length) {
+        descriptorElement.replaceWith += descriptor.charAt(charIndex);
+        charIndex++;
+        setTimeout(typewriter(), 1000); // Adjust the speed as needed
+      } else {
+        index++;
+        charIndex = 0;
+        setTimeout(typewriter(), 1000); // Adjust the delay between descriptors
+      }
+    }
+  };
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
@@ -19,10 +49,21 @@ const Hero = () => {
             Hey! &nbsp;
             <span className="text-[#915eff]"> I'm Ayan,</span>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              A Developer
+              <p id="myDescription">{typewriter()}</p>
             </p>
           </h1>
         </div>
+        <img
+          src={ayanhero}
+          alt="ayan-hero-img"
+          height={20}
+          width={180}
+          style={{
+            filter: "blur(0.6px) grayscale(0.8) ",
+            fillOpacity: "30%",
+            borderRadius: "50%",
+          }}
+        />
       </div>
       <ComputersCanvas />
       <div className="absolute sm:bottom-10 bottom-20 flex w-full justify-center items-center">
