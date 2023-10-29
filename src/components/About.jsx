2855bde1bb92686820da035fe67cc19955f11aae
ayanmta <mehtaayan18@gmail.com>
2023-10-29 21:17:53 +0530

    minor fixes stage 3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { styles } from "../style";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { ayanhero } from "../assets";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className="xs:w-[250px] w-full">
@@ -41,19 +42,33 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
-
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        id="about"
-      >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
-      </motion.p>
-
+      <div className="flex xs:flex-row flex-col">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          id="about"
+        >
+          I'm a skilled software developer with experience in TypeScript and
+          JavaScript, and expertise in frameworks like React, Node.js, and
+          Three.js. I'm a quick learner and collaborate closely with clients to
+          create efficient, scalable, and user-friendly solutions that solve
+          real-world problems. Let's work together to bring your ideas to life!
+        </motion.p>
+        <span className="xs:-mt-20">
+          {" "}
+          <img
+            src={ayanhero}
+            alt="ayan-hero-img"
+            height={20}
+            width={180}
+            style={{
+              filter: "blur(0.6px) grayscale(0.8) ",
+              fillOpacity: "30%",
+              borderRadius: "10%",
+            }}
+          />
+        </span>
+      </div>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ import { styles } from "../style";
 import { ComputersCanvas } from "./canvas";
 import { ayanhero } from "../assets";
 import "./../index.css";
+import { Typewriter } from "react-simple-typewriter";
 
 const Hero = () => {
   const [descriptors, setDescriptors] = useState([
@@ -22,7 +23,7 @@ const Hero = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prev) => (prev + 1) % descriptors.length);
-    }, 3500);
+    }, 3000);
     return () => {
       clearInterval(interval);
     };
@@ -31,14 +32,16 @@ const Hero = () => {
   useEffect(() => {
     const cinterval = setInterval(() => {
       setCursor((previsCursor) => !previsCursor);
-    }, 450);
-    return () => clearInterval(cinterval);
+    }, 300);
+    return () => {
+      clearInterval(cinterval);
+    };
   }, []);
 
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row  items-start gap-5`}
+        className={`${styles.paddingX} pb-85 sm:space-x-20 xs:-space-x-20 absolute inset-0 top-[120px] bottom-40 max-w-7xl mx-auto flex flex-row  items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -50,38 +53,32 @@ const Hero = () => {
             Hey! &nbsp;
             <span className="text-[#915eff]"> I'm Ayan,</span>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              <span className="block">
-                <span className="block overflow-hidden whitespace-no-wrap">
-                  <span
-                    className="type-write"
-                    style={{
-                      display: "inline-block",
-                      width: "0",
-                      animation:
-                        "typing 3.5s steps(12, end) infinite, blink-caret 0.75s step-end infinite",
-                    }}
-                  >
-                    {descriptors[currentIndex]}
-                    {isCursor ? "|" : ""}
-                  </span>
-                </span>
+              <span style={{ color: "red", fontWeight: "bold" }}>
+                {/* Style will be inherited from the parent element */}
+                <Typewriter
+                  words={[
+                    "A Full-Stack Developer 🚀",
+                    "An Orchardist 🌳",
+                    "A Physics Enthusiast ⚛️",
+                    "A Painter of Dreams 🎨",
+                    "loves Exploring the Cosmos 🌌",
+                    "A Mountain Nomad ⛰️",
+                    "A Trekker Extraordinaire 🌄",
+                    "loves Music 🎵",
+                  ]}
+                  loop={5}
+                  cursor
+                  cursorStyle="_"
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={200}
+                />
               </span>
             </p>
           </h1>
         </div>
-        <img
-          src={ayanhero}
-          alt="ayan-hero-img"
-          height={20}
-          width={180}
-          style={{
-            filter: "blur(0.6px) grayscale(0.8) ",
-            fillOpacity: "30%",
-            borderRadius: "10%",
-          }}
-        />
       </div>
-      <ComputersCanvas />
+
       <div className="absolute sm:bottom-10 bottom-20 flex w-full justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] border-4 rounded-3xl border-secondary flex items-start justify-center p-2">
@@ -97,6 +94,8 @@ const Hero = () => {
           </div>
         </a>
       </div>
+
+      <ComputersCanvas className="xs:block hidden" />
     </section>
   );
 };
